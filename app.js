b9d733094b059c3c51f5a7fff50e48d6af58fa9d
Ion Gorincioi <gorincioii@yahoo.com>
2016-08-11 13:03:10 -0700

    Npm path, updated app.js and index.html

--- app.js
+++ app.js
@@ -1,17 +1,37 @@
+// // require and instantiate express
+// var express = require('express');
+// var app = express();
+
+// app.get('/', function(req, res){
+//   // route to serve up the homepage (index.html)
+// });
+
+// app.post('/api/shorten', function(req, res){
+//   // route to create and return a shortened URL given a long URL
+// });
+
+// app.get('/:encoded_id', function(req, res){
+//   // route to redirect the visitor to their original URL given the short URL
+// });
+
+// var server = app.listen(3000, function(){
+//   console.log('Server listening on port 3000');
+// });
+
+
+
 // require and instantiate express
 var express = require('express');
 var app = express();
+// we'll need the path module to correctly concatenate our paths
+var path = require('path');
+
+// tell Express to serve files from our public folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
   // route to serve up the homepage (index.html)
-});
-
-app.post('/api/shorten', function(req, res){
-  // route to create and return a shortened URL given a long URL
-});
-
-app.get('/:encoded_id', function(req, res){
-  // route to redirect the visitor to their original URL given the short URL
+  res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
 var server = app.listen(3000, function(){
