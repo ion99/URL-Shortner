0894b1d3d0a33df4303dc485e34d3154442a9475
Ion Gorincioi <gorincioii@yahoo.com>
2016-08-11 14:03:31 -0700

    Instaled the middleware body-parser and configure the app.js to use it

--- app.js
+++ app.js
@@ -1,37 +1,74 @@
-// // require and instantiate express
-// var express = require('express');
-// var app = express();
 
-// app.get('/', function(req, res){
-//   // route to serve up the homepage (index.html)
-// });
-
-// app.post('/api/shorten', function(req, res){
-//   // route to create and return a shortened URL given a long URL
-// });
-
-// app.get('/:encoded_id', function(req, res){
-//   // route to redirect the visitor to their original URL given the short URL
-// });
-
-// var server = app.listen(3000, function(){
-//   console.log('Server listening on port 3000');
-// });
-
-
-
-// require and instantiate express
 var express = require('express');
 var app = express();
-// we'll need the path module to correctly concatenate our paths
 var path = require('path');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var config = require('./config');
+var base58 = require('./base58.js');
 
-// tell Express to serve files from our public folder
+// grab the url model
+var Url = require('./models/url');
+
+mongoose.connect('mongodb://' + config.db.host + '/' + config.db.name);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
-  // route to serve up the homepage (index.html)
   res.sendFile(path.join(__dirname, 'views/index.html'));
+});
+
+app.post('/api/shorten', function(req, res){
+  var longUrl = req.body.url;
+  var shortUrl = '';
+
+  // check if url already exists in database
+  Url.findOne({long_url: longUrl}, function (err, doc){
+    if (doc){
+      shortUrl = config.webhost + base58.encode(doc._id);
+
+      // the document exists, so we return it without creating a new entry
+      res.send({'shortUrl': shortUrl});
+    } else {
+      // since it doesn't exist, let's go ahead and create it:
+      var newUrl = Url({
+        long_url: longUrl
+      });
+
+      // save the new link
+      newUrl.save(function(err) {
+        if (err){
+          console.log(err);
+        }
+
+        shortUrl = config.webhost + base58.encode(newUrl._id);
+
+        res.send({'shortUrl': shortUrl});
+      });
+    }
+
+  });
+
+});
+
+app.get('/:encoded_id', function(req, res){
+
+  var base58Id = req.params.encoded_id;
+
+  var id = base58.decode(base58Id);
+
+  // check if url already exists in database
+  Url.findOne({_id: id}, function (err, doc){
+    if (doc) {
+      res.redirect(doc.long_url);
+    } else {
+      res.redirect(config.webhost);
+    }
+  });
+
 });
 
 var server = app.listen(3000, function(){
